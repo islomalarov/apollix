d7fe40d6353176239ede6f6888e2342e2820e0d3
Islom <islomalarov@gmail.com>
2025-11-27 16:55:51 +0500

    feat: add carousel

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Battery, ChevronRight } from 'lucide-react';
+import { ChevronRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Hero from '@/public/hero.jpg';
+import Hero2 from '@/public/hero-2.png';
+import Autoplay from 'embla-carousel-autoplay';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 interface HeroProps {
   isVisible: boolean;
   onScroll: (e: React.MouseEvent<HTMLButtonElement>, targetId: string) => void;
@@ -65,8 +68,24 @@ export function HeroSection({ isVisible, onScroll }: HeroProps) {
               isVisible ? 'translate-x-0 opacity-100' : 'translate-x-12 opacity-0'
             }`}>
             <div className="relative overflow-hidden rounded-3xl">
-              <div className="absolute inset-0 bg-linear-to-br from-primary/15 to-transparent rounded-3xl border border-primary/20"></div>
-              <Image src={Hero} alt="" className="w-full h-full object-cover" />
+              {/* <div className="absolute inset-0 bg-linear-to-br from-primary/15 to-transparent rounded-3xl border border-primary/20"></div> */}
+              {/* <Image src={Hero2} alt="" className="w-full h-full object-cover" /> */}
+              <Carousel
+                className=""
+                plugins={[
+                  Autoplay({
+                    delay: 5000,
+                  }),
+                ]}>
+                <CarouselContent>
+                  <CarouselItem>
+                    <Image src={Hero} alt="" className="w-full h-full object-cover" />
+                  </CarouselItem>
+                  <CarouselItem>
+                    <Image src={Hero2} alt="" className="w-full h-full object-cover" />
+                  </CarouselItem>
+                </CarouselContent>
+              </Carousel>
             </div>
           </div>
         </div>
